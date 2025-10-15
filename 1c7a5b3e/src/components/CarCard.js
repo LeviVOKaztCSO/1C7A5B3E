c4a5dea46af17d93350815c356ff7b74e96de33d
LeviVOKaztCSO <bartfai.levente@ganziskola.hu>
2025-10-15 17:22:37 +0200

    Delete 1c7a5b3e directory

--- 1c7a5b3e/src/components/CarCard.js
+++
@@ -1,9 +0,0 @@
-export default function CarCard({ car }) {
-  return (
-    <div className="car-card">
-      <h2>{car.brand} {car.model}</h2>
-      <p><strong>Évjárat:</strong> {car.year}</p>
-      <p><strong>Ár:</strong> {car.price}</p>
-    </div>
-  );
-}
